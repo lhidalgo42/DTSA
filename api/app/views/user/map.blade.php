141cc91e1ba36b1aabd1182efdbdcda556232ce9
Leonardo Hidalgo <leontuna@gmail.com>
2014-10-23 17:37:37 -0200

    working

--- api/app/views/user/map.blade.php
+++ api/app/views/user/map.blade.php
@@ -8,9 +8,17 @@
           </div>
                                  <!-- /.col-lg-12 -->
     </div>
-    <div class="row">
-        <img src="/img/map.PNG" width="100%" id="map" class="img-responsive" style="position: relative;">
-        <span style="position: absolute;">Hola</span>
+    <div class="row cycle-slideshow" id="site">
+        <img src="/img/map.PNG" width="100%" id="map">
+                <div id="text1">
+                    <strong><button class="btn btn-success"></button></strong> @{{ status 1 }}
+                </div>
+                <div id="text2">
+                    <strong><button class="btn btn-warning"></button></strong> @{{ status 2 }}
+                </div>
+                <div id="text3">
+                    <strong><button class="btn btn-danger"></button></strong> @{{ status 3 }}
+                </div>
     </div>
 </div>
         <!-- /#page-wrapper -->
@@ -23,4 +31,34 @@ $(document).ready(function(){
     wheelzoom($("#map"));
 });
 </script>
+@stop
+@section('css')
+<style>
+.cycle-slideshow {
+position: relative;
+z-index:1;
+
+}
+#text1 {
+    position: absolute;
+    top: 335px;
+    left: 800px;;
+    z-index:2;
+
+}
+#text2 {
+    position: absolute;
+    top: 350px;
+    left: 800px;;
+    z-index:2;
+
+}
+#text3 {
+    position: absolute;
+    top: 365px;
+    left: 800px;;
+    z-index:2;
+
+}
+</style>
 @stop
